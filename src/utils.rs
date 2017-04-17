0999d61935e82e725847d79562ec46e02934f121
Chris McDonald <mail.chris.mcdonald@gmail.com>
2017-04-17 05:20:19 -0600

    tests are gud

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 use ndarray::prelude::*;
 use rand::distributions::{Range, IndependentSample};
 use rand;
-
+use bit_vec::BitVec;
 
 pub fn rotate_90_clockwise<T: Copy>(image_data: &Array2<T>) -> Array2<T> {
     let mut output = image_data.t();
@@ -34,4 +34,45 @@ pub fn masked_weighted_choice<T, M>(input: &[(T, usize)], mask: &M) -> usize
         }
     }
     unreachable!();
+}
+
+pub fn mass_intersect(sets: Vec<BitVec>) -> Option<BitVec> {
+    let mut output = None;
+    for bv in sets {
+        match output {
+            None => {output = Some(bv);},
+            Some(ref mut v) => {v.intersect(&bv);}
+        }
+    }
+    output
+}
+
+
+#[test]
+fn mass_intersect_empty_test() {
+    let test_vec = vec![];
+    let output = mass_intersect(test_vec);
+    assert_eq!(output, None);
+}
+
+#[test]
+fn mass_intersect_single_test() {
+    let test_vec = vec![BitVec::from_elem(10, true)];
+    let output = mass_intersect(test_vec);
+    let result = Some(BitVec::from_elem(10, true));
+    assert_eq!(output, result);
+}
+
+#[test]
+fn mass_intersect_multi_test() {
+    let mut test_vec = vec![];
+    let test1 = BitVec::from_elem(8, true);
+    let test2 = BitVec::from_bytes(&[0b11110000,]);
+    let test3 = BitVec::from_bytes(&[0b11110011,]);
+    test_vec.push(test1);
+    test_vec.push(test2);
+    test_vec.push(test3);
+    let output = mass_intersect(test_vec);
+    let result = Some(BitVec::from_bytes(&[0b11110000,]));
+    assert_eq!(output, result);
 }
