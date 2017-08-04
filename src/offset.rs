7034ed0b097b68cf4008e9854ccc885cfc24ed34
Chris McDonald <mail.chris.mcdonald@gmail.com>
2017-08-04 03:14:29 -0600

    More general Offset2D impl

--- src/offset.rs
+++ src/offset.rs
@@ -1,6 +1,6 @@
 use std::convert::From;
 use std::ops::Neg;
-
+use num_traits::cast::NumCast;
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
 pub struct Offset2D(pub isize, pub isize);
@@ -12,14 +12,12 @@ impl Neg for Offset2D {
     }
 }
 
-impl From<(isize, isize)> for Offset2D {
-    fn from(src: (isize, isize)) -> Self {
-        Offset2D(src.0, src.1)
-    }
-}
-
-impl From<(usize, usize)> for Offset2D {
-    fn from(src: (usize, usize)) -> Self {
-        Offset2D(src.0 as isize, src.1 as isize)
+impl<T, U> From<(T, U)> for Offset2D
+where
+    T: NumCast,
+    U: NumCast,
+{
+    fn from(src: (T, U)) -> Self {
+        Offset2D(src.0.to_isize().unwrap(), src.1.to_isize().unwrap())
     }
 }
